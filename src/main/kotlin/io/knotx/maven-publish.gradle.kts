182e07bc1abf78df3722ee11421e88e03ccd5a33
Tomasz Michalak <tomek.michalak@gmail.com>
2019-06-13 13:24:44 +0200

    Knot.x gradle plugins

---
+++ src/main/kotlin/io/knotx/maven-publish.gradle.kts
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2019 Knot.x Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.knotx
+
+plugins {
+    `maven-publish`
+    signing
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set("${project.findProperty("publication.name")}")
+                description.set("${project.findProperty("publication.description")}")
+                url.set("http://knotx.io")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("marcinczeczko")
+                        name.set("Marcin Czeczko")
+                        email.set("https://github.com/marcinczeczko")
+                    }
+                    developer {
+                        id.set("skejven")
+                        name.set("Maciej Laskowski")
+                        email.set("https://github.com/Skejven")
+                    }
+                    developer {
+                        id.set("tomaszmichalak")
+                        name.set("Tomasz Michalak")
+                        email.set("https://github.com/tomaszmichalak")
+                    }
+                }
+                scm {
+                    connection.set("${project.findProperty("publication.scm")}")
+                    developerConnection.set("${project.findProperty("publication.scm")}")
+                    url.set("http://knotx.io")
+                }
+            }
+        }
+        repositories {
+            maven {
+                val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+                url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+                credentials {
+                    username = if (project.hasProperty("ossrhUsername")) project.property("ossrhUsername")?.toString() else "UNKNOWN"
+                    password = if (project.hasProperty("ossrhPassword")) project.property("ossrhPassword")?.toString() else "UNKNOWN"
+                    println("Connecting with user: ${username}")
+                }
+            }
+        }
+    }
+}
